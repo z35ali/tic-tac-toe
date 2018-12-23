3f3ec80ed5ebe9b01fc08a829c29921ed1f11563
Zafar Ali <zafar.a.ali2@gmail.com>
2018-12-22 23:54:45 -0500

    Fixed Bugs
    - Incorrect Vertical win with cells [0, 3, 5] instead of [0, 3, 6]
    - Game playable after winner is found

--- src/App.js
+++ src/App.js
@@ -13,47 +13,58 @@ class App extends Component {
       //initialize no winner
       gameEnded: false,
 
-      winner: undefined,
+      winner: undefined
 
+
+
+    }
+    this.gameState={
       //initialize board
       board: Array(9).fill(''),
       totalMoves: 0
-
     }
 
   }
 
   clicked(event){
 
-    //Only change empty cells
-    if(this.state.board[event.target.dataset.square] === ''){
-    this.state.board[event.target.dataset.square]=this.state.turn;
+  if(this.state.gameEnded!==true){
+    if(this.gameState.board[event.target.dataset.square] === ''){
+    this.gameState.board[event.target.dataset.square]=this.state.turn;
     event.target.innerText=this.state.turn;
     this.setState({
 
       //alternate between X and O
       turn: this.state.turn==='X' ? 'O' : 'X',
 
-      //update the board
-      board: this.state.board,
-
-      totalMoves: this.state.totalMoves++
     })
+      this.gameState.totalMoves++;
+
+}
 }
 
 var result= this.checkWinner();
   if(result === 'X'){
     this.setState({
       gameEnded: true,
-      winner: 'X'
+      winner: 'X',
+        winnerLine: 'Match won by X'
     });
     console.log('yup');
   }else if(result === 'O'){
     this.setState({
       gameEnded: true,
-      winner: 'O'
+      winner: 'O',
+      winnerLine: 'Match won by O'
 
   });
+}else if(result === 'draw'){
+  this.setState({
+    gameEnded: true,
+    winner: 'Draw',
+    winnerLine: 'Match is Drawn'
+
+});
 }
 
 
@@ -63,11 +74,14 @@ var result= this.checkWinner();
   checkWinner(){
 
     //set of all possible winning moves
-    var moves=[[0,3,5],[1,4,7], [2,5,8], [0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8]];
-    var board=this.state.board;
+    var moves=[[0,3,6],[1,4,7], [2,5,8], [0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8]];
+    var board=this.gameState.board;
     for (let i=0;i<moves.length; i++){
       if(board[moves[i][0]]===board[moves[i][1]] && board[moves[i][1]] === board[moves[i][2]]){
         return board[moves[i][0]];
+      }
+      if(this.gameState.totalMoves===9){
+        return 'draw';
       }
     }
 
@@ -77,6 +91,7 @@ var result= this.checkWinner();
   render() {
     return (
       <div id="game">
+      <div id="status">{this.state.winnerLine}</div>
         <div id="head">Play Tic Tac Toe vs AI
         </div>
         <div id="board" onClick={(e)=>this.clicked(e)}>
